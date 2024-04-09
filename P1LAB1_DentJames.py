e81293a2e9f6eab0db71962c1749f44d92c51f33
dentj8427 <166546646+dentj8427@users.noreply.github.com>
2024-04-09 14:14:12 -0400

    Add files via upload

---
+++ P1LAB1_DentJames.py
@@ -0,0 +1,9 @@
+
+#James Dent
+#4/9/2024
+#P1LAB1
+#PYTHON INTRODUCTION
+
+firstname=input("Enter your first name:")
+lastname=input("Enter your last name:")
+print(F"Hello,{firstname} {lastname}! ", "Welcome to Python")
