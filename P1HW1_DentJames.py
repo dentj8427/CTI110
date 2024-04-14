147c8f7b1d36f448b6d977315681a09e3918ad99
dentj8427 <166546646+dentj8427@users.noreply.github.com>
2024-04-14 11:45:25 -0400

    Add files via upload

---
+++ P1HW1_DentJames.py
@@ -0,0 +1,34 @@
+
+# James Dent
+# April 14, 2024
+# P1HW1
+# Introduction to Python 3
+# Writing a code that collects information from user and processes information 
+
+
+
+
+
+
+
+
+
+user_num = int(input('Enter integer:\n'))
+
+print("You entered:",user_num)
+
+squared = user_num **2
+
+print(user_num, "squared is",squared)
+
+print("And",user_num ,"cubed is", user_num**3,"!!")
+
+num2 = int(input('Enter another integer:\n'))
+
+adding = user_num + num2
+
+print(user_num, "+",num2,"is",adding)
+
+multiply = user_num * num2
+
+print(user_num, "*",num2,"is",multiply)
